a45fcf052993a91a8b4e06574b79f9fa987ccd5a
Damian Grzeszczuk <damian.grzeszczuk@gmail.com>
2017-04-03 16:36:48 +0200

    lock unlock

--- client/monitor.h
+++ client/monitor.h
@@ -8,14 +8,35 @@
 #ifndef DISTRIBUTEDMONITOR_MONITOR_H
 #define DISTRIBUTEDMONITOR_MONITOR_H
 
-#endif //DISTRIBUTEDMONITOR_MONITOR_H
+#define MSG_TYPE_ACK 0x1
+#define MSG_TYPE_REQUEST 0x2
+#define MSG_TYPE_RELEASE 0x3
+#define MSG_TYPE_SIGNAL 0x4
+
+#define PROTOCOL "tcp"
+
+#define LOCAL_REQUEST_PORT "5570"
+#define LOCAL_PUB_PORT "5571"
+
+#define LOCAL_REQUEST_URL PROTOCOL "://localhost" LOCAL_REQUEST_PORT
+#define LOCAL_PUB_URL PROTOCOL "://localhost" LOCAL_PUB_PORT
+
 using namespace std;
+struct Message {
+    int pid;
+    int time;
+    int type;
+    int mutexId;
+    int condId;
+    int data[100];
+};
 template <class T>
 class GenericMessage {
 public:
 
-    GenericMessage(int id, T msg):
+    GenericMessage(int id,int type, T msg):
             beId(id),
+            msg_type(type),
             data(msg)
     {}
 
@@ -44,8 +65,19 @@ public:
     {
         return beId;
     }
+
+    void setMsgType(int type)
+    {
+        msg_type = type;
+    }
+
+    int getMsgType()
+    {
+        return msg_type;
+    }
 private:
     int beId;
+    int msg_type;
     T data;
 };
 
@@ -54,6 +86,7 @@ class CompareQueue
 public:
     bool operator()(pair<int,int> n1,pair<int,int> n2) {
         return n1.second == n2.second ? n1.first < n2.first : n1.second < n2.second;
+        //second -> time; first -> pid
     }
 };
 
@@ -67,11 +100,12 @@ public:
         value = new int[arraySize]();
         prodMod = 0;
         consMod = 0;
+        ltime = 0;
         pid = procId;
         size = arraySize;
 //        sock_req_.setsockopt(ZMQ_IDENTITY, pid, sizeof(pid));
-        sock_req_.connect("tcp://localhost:5570");
-        sock_sub_.connect("tcp://localhost:5571");
+        sock_req_.connect(LOCAL_REQUEST_URL);
+        sock_sub_.connect(LOCAL_PUB_URL);
 
 //        sock_sub_.setsockopt(ZMQ_IDENTITY, pid, sizeof(pid));
 //        const char *filter = (argc > 1)? argv [1]: "ACK_SUB";
@@ -87,42 +121,104 @@ public:
 //    {
 //        return value;
 //    }
+
     void consume() {
-        lock();
+        lock(1);
         //pthread_mutex_lock(&m);
         while(value[consMod%size] == 0) {
-            wait();
-        //pthread_cond_wait(&c2, &m);
+            wait(2,1);
+            //pthread_cond_wait(&c2, &m);
         }
         value[consMod%size] = 0;
         consMod++;
         printf("%d CONSUMING VALUE AT %d\n",pid, consMod);
-        signal();
+        signal(1);
         //pthread_cond_signal(&c);
-        unlock();
+        unlock(1);
         //pthread_mutex_unlock(&m);
 
     }
-    void lock() {//mutexId
+    void produce() {
+        lock(1);
+        while(value[prodMod%size] == 0) {
+            wait(1,1);
+        }
+        value[prodMod%size] = 0;
+        prodMod++;
+        printf("%d PRODUCING VALUE AT %d\n",pid, prodMod);
+        signal(2);
+        unlock(1);
+    }
+    GenericMessage<pair<int,int>> prepare_message(int MSG_TYPE) {
+        pair<int,int> p = make_pair(pid,ltime);
+        GenericMessage<pair<int,int>> message = GenericMessage<pair<int,int>>(pid, MSG_TYPE, p);
+        return message;
+    }
+    zmq::message_t prepare_empty(int TYPE,int mutexId){
+        struct Message m;
+        m.pid = pid;
+        m.type = TYPE;
+        m.mutexId = mutexId;
+        m.time = ltime++; //++?
+        zmq::message_t msg (sizeof(struct Message));
+        memcpy (msg.data (), &m, (sizeof(struct Message)));
+        return msg;
+    }
+    zmq::message_t prepare_message(int TYPE, int mutexId, int condId) {
+        struct Message m;
+        m.pid = pid;
+        m.type = TYPE;
+        m.mutexId = mutexId;
+        m.condId = condId;
+        m.time = ltime++; //++?
+        memcpy(m.data, value, size*sizeof(int));
+        zmq::message_t msg (sizeof(struct Message));
+        memcpy (msg.data (), &m, (sizeof(struct Message)));
+        return msg;
+    }
+    void lock(int mutexId) {
         //request for mutex
+        sock_req_.send(prepare_empty(MSG_TYPE_REQUEST,mutexId));
         //wait for ack
-        //if got all ack, wait for signal/unlocks till first in queue
+        memset(ack, 0, PROC_NUM*sizeof(*ack)); //zero ack table
+        while(isWaiting()) {
+            //wait for signal GOT_ACK_MESSAGE and check again
+        }
+
+        //if got all ack, wait for ?signal?/unlocks till first in queue
+        while(pq.top().first != pid) {
+            //wait for signal GOT_RELEASE_MESSAGE and check again
+        }
     }
-    void wait() {
+    void wait(int condId, int mutexId) {
         //unlock mutex
-    //wait for signal
-        //lock()
+        unlock(mutexId);
+        //wait for signal
+        //pthread_wait
         //if got signal, wait for unlocks/signals till first in queue
+        lock(mutexId);
     }
-    void signal() { //with condId, mutexId, values
+    void signal(int condId) {
+        sock_req_.send(prepare_message(MSG_TYPE_SIGNAL,-1,condId));
+        //with condId, mutexId, values
         //port 5570 request with value, consMod, prodMod, dont need to wait
     }
-    void unlock() { //with mutexId, values
+    void unlock(int mutexId) {
+        pq.pop();
+        sock_req_.send(prepare_message(MSG_TYPE_RELEASE,mutexId, -1));
+        //with mutexId, values
         //port 5570 request with value, consMod, prodMod, dont need to wait
+    }
+    void handle_msg() {
+        //pthread process must loop this on sock_sub_.recv
+        //and switch on type
     }
 private:
     int *value;
+    int *ack;
     int pid;
+    int ltime;
+    int PROC_NUM;
 //    T value;
     int size;
     int prodMod;
@@ -131,4 +227,13 @@ private:
     zmq::socket_t sock_req_;
     zmq::socket_t sock_sub_;
     priority_queue<pair<int,int>,vector<pair<int,int>>,CompareQueue> pq;
+    bool isWaiting() {
+        for(int i =0;i<PROC_NUM;i++) {
+            if(ack[i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
+#endif //DISTRIBUTEDMONITOR_MONITOR_H
